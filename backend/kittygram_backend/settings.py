1704fa7afd7c33881dd9bbcc8e51e861e30563e3
Gleb <qtafy@MB-P.local>
2023-09-19 08:45:38 +0100

    review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,11 +2,18 @@
 import os
 from pathlib import Path
 
+import environ
+from dotenv import load_dotenv
+
+
+env = environ.Env()
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool('DEBUG_PROD', default=False)
 
 ALLOWED_HOSTS = []
 
